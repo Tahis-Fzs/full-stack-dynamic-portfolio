2cddd805ffc088b06155ac752ff65ee11ff4f596
Tahis-Fzs <tahsin22205101461@diu.edu.bd>
2025-10-25 13:47:14 +0600

    Added Blade layouts, pages, and partials structure

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.home');
+});
+
+Route::get('/skills', function () {
+    return view('pages.skills');
 });
